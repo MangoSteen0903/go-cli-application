e4125ba3a56427f477a66441ca96331cd9880087
MangoSteen0903 <sjlee990129@gmail.com>
2023-03-07 19:13:39 +0900

    Clear Project & Reconstruct

--- client-cli/cmd/errors.go
+++
@@ -1,7 +0,0 @@
-package cmd
-
-import "errors"
-
-var ErrNotServerSpecified = errors.New("server should be specified")
-var ErrInvalidSubCommand = errors.New("invalid sub-command")
-var ErrInvalidMethods = errors.New("methods invalid")
